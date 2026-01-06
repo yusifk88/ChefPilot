e1b5482858d4445e228c3514350dd41fae435fa6
yusifk88 <amloyal4life@gmail.com>
2026-01-06 17:26:12 -0500

    added logs

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -106,7 +106,7 @@ class ItemsController extends Controller
 
         Log::info("found data",["user_id" => $user->id, "item_id" => $item->id,"item" => $item,"bookmarked" => !$item->bookmarked]);
 
-        $item->update(["bookmarked" => !$item->bookmarked]);
+        $item->update(["bookmarked" => true]);
 
     }
 
