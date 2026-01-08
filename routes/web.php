fe29d3dbe1b222391fcb36760503e1497b445df3
yusifk88 <amloyal4life@gmail.com>
2026-01-07 20:37:07 -0500

    added android deeplink wellknown link

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,19 @@ use App\Http\Controllers\ItemsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/res/{ulid}', [ItemsController::class, 'publicPost'])->name('recipe.publicPost');
+
+
+Route::get("/.well-known/assetlinks.json", function () {
+
+    return json_decode('[
+  {
+    "relation": ["delegate_permission/common.handle_all_urls"],
+    "target": {
+      "namespace": "android_app",
+      "package_name": "com.chefpilot.app",
+      "sha256_cert_fingerprints": ["12:08:41:8D:3D:E4:17:2F:44:59:9A:44:A7:8F:CD:E0:11:99:A6:61:9B:D2:26:83:AC:B4:C9:22:E6:F9:80:4C"]
+    }
+  }
+]');
+
+});
