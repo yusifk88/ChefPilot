5bd9d49bd6fc978e4e7a693a56db4ae7447d46e3
yusifk88 <amloyal4life@gmail.com>
2026-01-29 11:50:16 -0500

    changed realtime provider to scaledrone

---
+++ app/Services/ScaleDrone.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\Services;
+
+use App\Models\Recipe;
+use App\Models\User;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class ScaleDrone
+{
+
+    public static function recipeCreated(User $user, Recipe $recipe)
+    {
+        $ID = "Yh4KOdyE8eyesTXu";
+
+        $url = config("scale_drone.url") . $ID . "/RecipeCreated_" . $user->id . "/publish";
+
+        $response = Http::withToken(config("scale_drone.key"))->post($url, (array)$recipe);
+
+        Log::info("scaledrone response", ["response" => $response->body()]);
+        return $response->object();
+    }
+
+}
---
+++ config/scale_drone.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    "url"=>env("SCALEDRONE_URL"),
+    "key"=>env("SCALEDRONE_KEY"),
+];
