b8d004c5247a71b84f35e276aecdaf87a894cfed
yusifk88 <amloyal4life@gmail.com>
2026-01-29 12:30:33 -0500

    changed scaledrone auth method

--- app/Services/ScaleDrone.php
+++ app/Services/ScaleDrone.php
@@ -4,19 +4,30 @@ namespace app\Services;
 
 use App\Models\Recipe;
 use App\Models\User;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class ScaleDrone
 {
 
+    /**
+     * @throws ConnectionException
+     */
     public static function recipeCreated(User $user, Recipe $recipe)
     {
         $ID = "Yh4KOdyE8eyesTXu";
 
         $url = config("scale_drone.url") ."/". $ID . "/RecipeCreated_" . $user->id . "/publish";
 
-        $response = Http::withToken(config("scale_drone.key"))->post($url, (array)$recipe);
+        $key =config("scale_drone.key");
+
+        $authToken = base64_encode("$ID:$key");
+
+        $response = Http::withHeaders([
+            "Authorization" => "Basic $authToken",
+            "Content-Type" => "application/json"
+        ])->post($url, (array)$recipe);
 
         Log::info("scaledrone response", ["response" => $response->body()]);
         return $response->status();
