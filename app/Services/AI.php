2b99ca8234e7339bdd76019f5a859a818099ddbc
yusifk88 <amloyal4life@gmail.com>
2026-01-29 15:44:03 -0500

    changed model

--- app/Services/AI.php
+++ app/Services/AI.php
@@ -86,7 +86,7 @@ PROMPT;
 
         $userPrompt = "Here is my food inventory for the week:" . $food->toJson() . "suggest some recipes for me";
 
-        $model = "openai/gpt-oss-20b";
+        $model = "qwen/qwen3-32b";
 
         $response2 = Http::withToken(config("openai.api_key"))
             ->post("https://api.groq.com/openai/v1/chat/completions", [
