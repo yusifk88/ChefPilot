3622623f4b7f8a6429b69e5f10a684cc4b2cc728
yusifk88 <amloyal4life@gmail.com>
2026-02-05 19:40:20 -0500

    changed fingerprint for live app

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get("/.well-known/assetlinks.json", function () {
     "target": {
       "namespace": "android_app",
       "package_name": "com.chefpilot.app",
-      "sha256_cert_fingerprints": ["12:08:41:8D:3D:E4:17:2F:44:59:9A:44:A7:8F:CD:E0:11:99:A6:61:9B:D2:26:83:AC:B4:C9:22:E6:F9:80:4C"]
+      "sha256_cert_fingerprints": ["71:1E:D6:44:B5:31:4D:7D:DE:35:3B:61:FC:2E:67:BE:F3:74:17:C4:76:A6:35:DC:88:8B:50:AD:97:58:C1:AD"]
     }
   }
 ]');
